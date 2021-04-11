bd6cf4d2591d5edd6ada8b257d12c27e9f5c8f0e
seonbinKim3050 <tjs3050@Daum.net>
2021-04-12 00:50:18 +0900

    kimseonbin

---
+++ pages/posts/first-post.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Hi. I'm seon bin Kim. I'm major in sport industry and information system. </h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
